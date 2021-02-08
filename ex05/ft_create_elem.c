aaaa5470bcf89267e16a001ead30ff2ea67115e7
Manuel Sousa <mlrcbsousa@gmail.com>
2021-02-08 01:37:02 +0100

    Exercise 05, create linked list from string array.

--- ex05/ft_create_elem.c
+++ ex05/ft_create_elem.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_push_strs.c                                :+:      :+:    :+:   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/08 00:53:58 by manuel            #+#    #+#             */
-/*   Updated: 2021/02/08 01:35:30 by manuel           ###   ########.fr       */
+/*   Created: 2021/02/07 19:23:32 by manuel            #+#    #+#             */
+/*   Updated: 2021/02/07 19:46:23 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-t_list	*ft_list_push_strs(int size, char **strs)
+t_list	*ft_create_elem(void *data)
 {
-	t_list	*list;
+	t_list	*elem;
 
-	if (size == 0)
-		return (0);
-	list = ft_create_elem(*(strs + size - 1));
-	list->next = ft_list_push_strs(size - 1, strs);
-	return (list);
+	elem = (t_list *)malloc(sizeof(t_list));
+	elem->data = data;
+	elem->next = NULL;
+	return (elem);
 }
