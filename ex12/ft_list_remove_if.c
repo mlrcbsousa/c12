a7a781e61a120ef4ed4a005d55a29dbcab62a950
Manuel Sousa <mlrcbsousa@gmail.com>
2021-02-10 23:25:39 +0100

    Change edge case for first element removal to compare equality instead of not NULL.

--- ex12/ft_list_remove_if.c
+++ ex12/ft_list_remove_if.c
@@ -6,7 +6,7 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 01:03:54 by manuel            #+#    #+#             */
-/*   Updated: 2021/02/10 01:57:08 by manuel           ###   ########.fr       */
+/*   Updated: 2021/02/10 23:16:04 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
 		if (!cmp(current->data, data_ref))
 		{
 			free_fct(current->data);
+			if (current == *begin_list)
+				*begin_list = next;
 			free(current);
 			if (previous)
 				previous->next = next;
-			if (!*begin_list)
-				*begin_list = next;
 		}
 		else
 			previous = current;
